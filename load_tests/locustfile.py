e59163ae7d1528bd85ca6c7678ead448bb3d8c89
nda00 <72438618+nda00@users.noreply.github.com>
2026-04-05 19:24:21 +0300

    Update locustfile.py

--- load_tests/locustfile.py
+++ load_tests/locustfile.py
@@ -1,14 +1,14 @@
 from locust import HttpUser, task, between
 
 class ProjectUser(HttpUser):
-    wait_time = between(1, 2) # Her istek arası 1-2 saniye bekle
+    wait_time = between(1, 2) 
 
     @task
     def get_user_test(self):
-        # BURADAKİ ID'Yİ KENDİ ID'NLE DEĞİŞTİR!
+    
         user_id = "69cbb60014286632cc0911c7"
         self.client.get(f"/users/users/{user_id}")
 
-    @task(2) # Bu görev 2 kat daha fazla çalışır
+    @task(2) 
     def health_check(self):
-        self.client.get("/users/health") # Varsa health check test eder
+        self.client.get("/users/health") 
